a8c5846ac1f1daeddeb0f3310a3f8415fe896053
Jayg82 <f5dwy89szm@privaterelay.appleid.com>
2026-09-12 01:31:42 -0400

    Document URP mobile performance setup

--- unity/Assets/Scripts/Core/MobileQualityBootstrap.cs
+++ unity/Assets/Scripts/Core/MobileQualityBootstrap.cs
@@ -6,6 +6,7 @@ namespace UticaDrive.Core
     {
         [SerializeField] int targetFps = 60;
         [SerializeField] bool batterySaver;
+        [SerializeField] bool enableSoftShadows = true;
         void Awake()
         {
             Application.targetFrameRate = batterySaver ? 30 : targetFps;
@@ -13,6 +14,15 @@ namespace UticaDrive.Core
             QualitySettings.anisotropicFiltering = AnisotropicFiltering.Enable;
             QualitySettings.shadowDistance = batterySaver ? 35f : 80f;
             QualitySettings.lodBias = batterySaver ? .65f : 1f;
+            QualitySettings.softParticles = true;
+            QualitySettings.realtimeReflectionProbes = false;
+            QualitySettings.shadowProjection = ShadowProjection.CloseFit;
+            QualitySettings.shadowCascades = batterySaver ? 0 : 2;
+            QualitySettings.shadowResolution = batterySaver ? ShadowResolution.Low : ShadowResolution.Medium;
+            QualitySettings.shadowDistance = batterySaver ? 28f : 65f;
+            if (enableSoftShadows && !batterySaver)
+                QualitySettings.globalTextureMipmapLimit = 0;
         }
     }
 }
+
