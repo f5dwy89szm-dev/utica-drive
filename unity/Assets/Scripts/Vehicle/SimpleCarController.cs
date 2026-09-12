92b0db75eea9ade0b2673be6492160e2aa64d0c2
Jayg82 <f5dwy89szm@privaterelay.appleid.com>
2026-09-12 01:29:56 -0400

    Add throttle joystick and mobile brake input

--- unity/Assets/Scripts/Vehicle/SimpleCarController.cs
+++ unity/Assets/Scripts/Vehicle/SimpleCarController.cs
@@ -9,6 +9,8 @@ namespace UticaDrive.Vehicle
         [SerializeField] WheelCollider[] driveWheels;
         [SerializeField] WheelCollider[] steerWheels;
         [SerializeField] VirtualJoystick steeringStick;
+        [SerializeField] VirtualJoystick throttleStick;
+        [SerializeField] MobileActionButton brakeButton;
         [SerializeField] float maxMotorTorque = 1800f;
         [SerializeField] float maxSteerAngle = 30f;
         [SerializeField] float maxBrakeTorque = 2600f;
@@ -24,13 +26,15 @@ namespace UticaDrive.Vehicle
         void FixedUpdate()
         {
             var steer = steeringStick ? steeringStick.Value.x : Input.GetAxis("Horizontal");
-            var throttle = Mathf.Clamp01(Input.GetAxis("Vertical"));
-            var brake = Mathf.Clamp01(-Input.GetAxis("Vertical"));
+            var inputY = throttleStick ? throttleStick.Value.y : Input.GetAxis("Vertical");
+            var throttle = Mathf.Clamp01(inputY);
+            var brake = Mathf.Clamp01(-inputY) + (brakeButton && brakeButton.Held ? 1f : 0f);
             var speed = body.linearVelocity.magnitude * 3.6f;
             var torque = speed < topSpeedKph ? throttle * maxMotorTorque : 0f;
             foreach (var wheel in driveWheels) wheel.motorTorque = torque;
             foreach (var wheel in steerWheels) wheel.steerAngle = steer * maxSteerAngle;
-            foreach (var wheel in driveWheels) wheel.brakeTorque = brake * maxBrakeTorque;
+            foreach (var wheel in driveWheels) wheel.brakeTorque = Mathf.Clamp01(brake) * maxBrakeTorque;
         }
     }
 }
+
